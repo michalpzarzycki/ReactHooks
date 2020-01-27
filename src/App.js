b5109c971a63db8c87b939b4d48bec3f44d11733
Michal Zarzycki <michal.p.zarzycki@wp.pl>
2020-01-27 23:40:20 +0100

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
   const [ value, setValue ] = useState(0);
-
+ useEffect(()=>{
+   document.title = `Kliknięto ${value} razy`
+ }, [value])
   return (
     <div className="App">
         <p>Kliknięto {value} razy</p>
