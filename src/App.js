2c3e16a1e5a7f207c153103536ae60851ea54bb3
Michal Zarzycki <michal.p.zarzycki@wp.pl>
2020-01-27 23:38:02 +0100

    useState

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [ value, setValue ] = useState(0);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <p>Kliknięto {value} razy</p>
+      <button onClick={() => setValue(value + 1)}>
+        Kliknij mnie
+      </button>
     </div>
   );
 }
